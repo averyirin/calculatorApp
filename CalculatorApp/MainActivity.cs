2d8fee16fa47476aae4b110455a45b14080bbe96
Irin Avery <Irin Avery@DESKTOP-9JJRKV3>
2017-01-20 18:42:11 -0500

    Add project files.

---
+++ CalculatorApp/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CalculatorApp {
+    [Activity(Label = "CalculatorApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
