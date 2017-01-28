ad8948d7b3a5439943d2183869c85e0a019e352b
Irin Avery <Irin Avery@DESKTOP-9JJRKV3>
2017-01-28 10:25:14 -0500

    Created front-end gui interface

--- CalculatorApp/Resources/Resource.Designer.cs
+++ CalculatorApp/Resources/Resource.Designer.cs
@@ -60,14 +60,8 @@ namespace CalculatorApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int button1 = 2131034114;
-			
-			// aapt resource value: 0x7f050001
-			public const int gridLayout1 = 2131034113;
-			
 			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			public const int myButton = 2131034112;
 			
 			static Id()
 			{
