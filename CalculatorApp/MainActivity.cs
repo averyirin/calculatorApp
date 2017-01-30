011b6b3acefa1dac1f23bcd09389c6aa14e5fb3b
Irin Avery <Irin Avery@DESKTOP-9JJRKV3>
2017-01-30 14:13:18 -0500

    Created backend buttons and text to create functions with

--- CalculatorApp/MainActivity.cs
+++ CalculatorApp/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace CalculatorApp {
     [Activity(Label = "CalculatorApp", MainLauncher = true, Icon = "@drawable/icon")]
@@ -10,7 +11,46 @@ namespace CalculatorApp {
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+
+
+            // Get all of our number buttons
+            Button btn1 = (Button)FindViewById(Resource.Id.btn1);
+            Button btn2 = (Button)FindViewById(Resource.Id.btn2);
+            Button btn3 = (Button)FindViewById(Resource.Id.btn3);
+            Button btn4 = (Button)FindViewById(Resource.Id.btn4);
+            Button btn5 = (Button)FindViewById(Resource.Id.btn5);
+            Button btn6 = (Button)FindViewById(Resource.Id.btn6);
+            Button btn7 = (Button)FindViewById(Resource.Id.btn7);
+            Button btn8 = (Button)FindViewById(Resource.Id.btn8);
+            Button btn9 = (Button)FindViewById(Resource.Id.btn9);
+            Button btn0 = (Button)FindViewById(Resource.Id.btn0);
+
+            //function buttons
+           Button btnAdd = (Button)FindViewById(Resource.Id.btnAdd);
+           Button btnSubtract = (Button)FindViewById(Resource.Id.btnSubtract);
+           Button btnMultiply = (Button)FindViewById(Resource.Id.btnMultiply);
+           Button btnDivide = (Button)FindViewById(Resource.Id.btnDivide);
+           Button btnEquals = (Button)FindViewById(Resource.Id.btnEquals);
+           Button btnClear = (Button)FindViewById(Resource.Id.btnClear);
+           Button btnDelete = (Button)FindViewById(Resource.Id.btnDelete);
+           Button btnRoot = (Button)FindViewById(Resource.Id.btnRoot);
+           Button btnSquared = (Button)FindViewById(Resource.Id.btnSquared);
+           Button btnPower = (Button)FindViewById(Resource.Id.btnPower);
+           Button btnDecimal = (Button)FindViewById(Resource.Id.btnDecimal);
+           Button btnPlusMinus = (Button)FindViewById(Resource.Id.btnPlusMinus);
+           Button btnPercent = (Button)FindViewById(Resource.Id.btnPercent);
+
+
+            //display text
+            TextView textDisplay = (TextView) FindViewById(Resource.Id.textDisplay);
+           
+
+            /* Button Clear */
+            btnClear.Click += delegate {
+                textDisplay.Text = "";
+            };
         }
+
     }
 
 }
--- CalculatorApp/Resources/Resource.Designer.cs
+++ CalculatorApp/Resources/Resource.Designer.cs
@@ -60,8 +60,77 @@ namespace CalculatorApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050015
+			public const int btn0 = 2131034133;
+			
+			// aapt resource value: 0x7f050011
+			public const int btn1 = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int btn2 = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int btn3 = 2131034131;
+			
+			// aapt resource value: 0x7f05000d
+			public const int btn4 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int btn5 = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int btn6 = 2131034127;
+			
+			// aapt resource value: 0x7f050009
+			public const int btn7 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int btn8 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int btn9 = 2131034123;
+			
+			// aapt resource value: 0x7f050014
+			public const int btnAdd = 2131034132;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnClear = 2131034117;
+			
+			// aapt resource value: 0x7f050016
+			public const int btnDecimal = 2131034134;
+			
+			// aapt resource value: 0x7f050001
+			public const int btnDelete = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnDivide = 2131034120;
+			
+			// aapt resource value: 0x7f050017
+			public const int btnEquals = 2131034135;
+			
+			// aapt resource value: 0x7f05000c
+			public const int btnMultiply = 2131034124;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnPercent = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int btnPlusMinus = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnPower = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int btnRoot = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int btnSquared = 2131034115;
+			
+			// aapt resource value: 0x7f050010
+			public const int btnSubtract = 2131034128;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int textDisplay = 2131034112;
 			
 			static Id()
 			{
